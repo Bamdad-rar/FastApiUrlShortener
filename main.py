805526f07af26fd7498d8af1571ab738e9aeefbd
b.abbasian <bamdad.rar@gmail.com>
2023-03-31 12:31:51 +0330

    implemented basic shortening and resolving urls

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-from fastapi import FastAPI
+import hashlib
+from typing import Annotated
+
+from fastapi import Body, FastAPI, HTTPException, Response
 
 app = FastAPI()
 
 
-dict_of_urls = dict()
+url_pool = dict()
 
 
 @app.get("/all")
 def root():
-    """ returns all shortened urls """
-    return dict_of_urls
+    """returns all shortened urls"""
+    return url_pool
 
 
-@app.post("/shorten")
-def shorten():
-    # shorten the url based on some logic...
-    # return the shortened version
-    # save the key, pair value for later retrieval / usage
-    shortened_version = "something here"
-    return shortened_version
+@app.post("/shorten/")
+def shorten(url: Annotated[str, Body(embed=True)]):
+    # the most usual way of shortening the url based on a search i did was, hashing it by either md5 or sha256 then use that (or part of it) as the shortened url
+    for short_url, real_url in url_pool.items():
+        if url == real_url:
+            return short_url
+
+    code = hashlib.sha256(url.encode()).hexdigest()[:7]
+    url_pool[code] = url
+    return code
 
 
-@app.get("/{shortened_url}")
-def resolve():
-    # get the real url of the shortened url
-    # redirect page to the real url
-    pass
+@app.get("/{code}")
+def resolve(code: str, response: Response):
+    if real_url := url_pool.get(code):
+        response.headers["Location"] = real_url
+        response.status_code = 301
+    else:
+        raise HTTPException(status_code=404, detail="URL NOT FOUND")
